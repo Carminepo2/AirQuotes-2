9d2475ef1d0fc26163190b8e5ebb5410a36d7ace
Carmine Porricelli <carmineporricelli96@gmail.com>
2022-01-31 16:30:52 +0100

    Initial Commit

---
+++ AirQuotes/AirQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirQuotesApp.swift
+//  AirQuotes
+//
+//  Created by Carmine Porricelli on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct AirQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirQuotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AirQuotes
+//
+//  Created by Carmine Porricelli on 31/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
