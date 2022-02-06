c8ab813ecd4fbacf93bb124efe234d2024db1f15
riccardoruocco <ruocco.rf@gmail.com>
2022-02-06 23:53:33 +0100

    [ADD] StoreAirQuotes headers

--- AirQuotes/ViewControllers/StoreAirQuotes.swift
+++ AirQuotes/ViewControllers/StoreAirQuotes.swift
@@ -17,6 +17,46 @@ class StoreAirQuotes: ObservableObject {
         model = ModelAirQuotes()
     }
     
+    func createTag(name:String,color:String){
+        
+    }
+    func removeTag(id:UUID){
+        
+    }
+    func createFolder(folderName:String){
+        
+    }
+    func removeFolder(id:UUID){
+        
+    }
+    func createQuote(text:String,authorName:String,parentFolder:UUID,tagList:Array<Tag>){
+        
+    }
+    func deleteQuote(id:UUID){
+        
+    }
+    func updateQuote(id:UUID,text:String,authorName:String,tagList:Array<Tag>){
+        
+    }
+    func getAllFolders() ->Array<Folder> {
+        
+        return []
+    }
+    func getAllQuotesInFolder(folder:UUID)->Array<Quote>{
+        return []
+    }
+    func addQuoteToFavorites(quote:UUID){
+        
+    }
+    func removeQuoteFromFavorites(quoteToRemove:UUID){
+        
+    }
+    func getAllQuotes()->Array<Quote>{
+        return []
+    }
+    func searchByTag(tag:UUID){
+        
+    }
     
     
 }
