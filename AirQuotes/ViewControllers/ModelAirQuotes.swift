56d5cf6ab759cbc53674b7319eff0bccd8aa3f47
riccardoruocco <ruocco.rf@gmail.com>
2022-02-07 10:15:35 +0100

    [ADD] func to ModelAirQuotes

--- AirQuotes/ViewControllers/ModelAirQuotes.swift
+++ AirQuotes/ViewControllers/ModelAirQuotes.swift
@@ -22,4 +22,41 @@ struct ModelAirQuotes{
         
     }
     
+    mutating func createTag(name:String,color:String){
+        
+    }
+    mutating func removeTag(id:UUID){
+        
+    }
+    mutating func createFolder(folderName:String){
+        
+    }
+    mutating func removeFolder(id:UUID){
+        
+    }
+    mutating func createQuote(text:String,authorName:String,parentFolder:UUID,tagList:Array<Tag>){
+        
+    }
+    mutating func deleteQuote(id:UUID){
+        
+    }
+    mutating func updateQuote(id:UUID,text:String,authorName:String,tagList:Array<Tag>){
+    
+    }
+    mutating func getFolder(idFolder:UUID){
+        
+    }
+    mutating func addQuoteToFavorites(idQuote:UUID){
+        
+    }
+    mutating func removeQuoteFromFavorites(idQuote:UUID){
+        
+    }
+    
+    
+}
+extension Quote{
+    func setQuote(text:String,authorName:String,parentFolder:UUID,tagList:Array<Tag>){
+        
+    }
 }
