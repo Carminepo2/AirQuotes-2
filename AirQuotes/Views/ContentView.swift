161188a90a547a6e9c6e37d4f44e4cc39c552e7b
riccardoruocco <ruocco.rf@gmail.com>
2022-02-15 23:48:06 +0100

    minor

--- AirQuotes/Views/ContentView.swift
+++ AirQuotes/Views/ContentView.swift
@@ -11,7 +11,6 @@ struct ContentView: View {
     var body: some View {
         
         TabView {
-            
             //MARK: - Latest View Tab
             NavigationView {
                 LatestView()
--- AirQuotes/Views/LatestTab/QuotesSliderView.swift
+++ AirQuotes/Views/LatestTab/QuotesSliderView.swift
@@ -11,7 +11,7 @@ struct QuoteSliderView: View {
     @EnvironmentObject var Controller: StoreAirQuotes
     
       var latestQuotes: Array<Quote> {
-        return Controller.getLatestQuotes()
+          return StoreAirQuotes.shared.getLatestQuotes()
     }
     
     var body: some View {
